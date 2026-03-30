8a3aa110b5ff8d3c8a9a81cbdb2b7a9b53eff246
poedchong-c <poedchong_c@silpakorn.edu>
2026-03-30 20:03:27 +0700

    Add files via upload

---
+++ word_202603301956.sql
@@ -0,0 +1,29 @@
+INSERT INTO dictionary.word (Column1,Column2,Column3,Column4,Column5) VALUES
+	 ('id','vocab','meaning','category_id','gif_url'),
+	 ('1','สวัสดี','Hello','1','https://www.th-sl.com//wp-content/uploads/2020/09/1.1.1.gif'),
+	 ('2','ขอบคุณ','Thank you','1','https://www.th-sl.com//wp-content/uploads/2020/09/1.2.1.gif'),
+	 ('3','สบายดี','I am fine','1','https://www.th-sl.com//wp-content/uploads/2021/10/4.098.gif'),
+	 ('4','ยินดีด้วย','Welcome','1','https://www.th-sl.com//wp-content/uploads/2023/12/2566-0945.gif'),
+	 ('5','ทำไม','Why?','2','https://www.th-sl.com//wp-content/uploads/2020/09/1.56.1.gif'),
+	 ('6','กี่โมง','What time?','2','https://www.th-sl.com//wp-content/uploads/2021/10/6.041-02.gif'),
+	 ('7','อะไร','What?','2','https://www.th-sl.com//wp-content/uploads/2020/09/1.55.1.gif'),
+	 ('8','หนึ่ง','One','3','https://www.th-sl.com//wp-content/uploads/2021/05/2.100.1.gif'),
+	 ('9','สอง','Two','3','https://www.th-sl.com//wp-content/uploads/2021/05/2.100.2.gif');
+INSERT INTO dictionary.word (Column1,Column2,Column3,Column4,Column5) VALUES
+	 ('10','สาม','Three','3','https://www.th-sl.com//wp-content/uploads/2021/05/2.100.3.gif'),
+	 ('11','สี่','Four','3','https://www.th-sl.com//wp-content/uploads/2021/05/2.100.4.gif'),
+	 ('12','ห้า','Five','3','https://www.th-sl.com//wp-content/uploads/2021/05/2.100.5.gif'),
+	 ('13','ตอนเช้า','Morning','4','https://www.th-sl.com//wp-content/uploads/2020/09/1.35.2.gif'),
+	 ('14','ตอนเย็น','Evening','4','https://www.th-sl.com//wp-content/uploads/2020/09/1.37.1.gif'),
+	 ('15','เที่ยงวัน','Noon','4','https://www.th-sl.com//wp-content/uploads/2021/06/2.100.47.gif'),
+	 ('16','เที่ยงคืน','Midnight','4','https://www.th-sl.com//wp-content/uploads/2025/06/1.38.2-2.gif'),
+	 ('17','หมั่นไส้','dislike','5','https://www.th-sl.com//wp-content/uploads/2025/11/2568-0383.gif'),
+	 ('18','เบื่อ','bored','5','https://www.th-sl.com//wp-content/uploads/2020/09/5.87.1.gif'),
+	 ('19','โกรธ','angry','5','https://www.th-sl.com//wp-content/uploads/2020/09/5.72.1.gif');
+INSERT INTO dictionary.word (Column1,Column2,Column3,Column4,Column5) VALUES
+	 ('20','เสียใจ','sad','5','https://www.th-sl.com//wp-content/uploads/2020/09/5.83.2.gif'),
+	 ('21','ดีใจ','happy','5','https://www.th-sl.com//wp-content/uploads/2020/09/5.83.1.gif'),
+	 ('22','ข้าวไก่ทอด','fried chicken rice','6','https://www.th-sl.com//wp-content/uploads/2025/08/2568-0295.gif'),
+	 ('23','กะเพรา','kaphrao','6','https://www.th-sl.com//wp-content/uploads/2020/09/3.13.2.gif'),
+	 ('24','ไข่เจียว','omelette','6','https://www.th-sl.com//wp-content/uploads/2023/11/5-4-1.gif'),
+	 ('25','ผัดไทย','padthai','6','https://www.th-sl.com//wp-content/uploads/2020/09/5.15.2.gif');
